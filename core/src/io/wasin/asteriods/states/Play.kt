3ec53cf5b22cedac35de8cd448101170ca87bb63
Wasin Thonkaew <haxpor@gmail.com>
2017-07-09 23:42:30 +0700

    Add player and drew it on screen facing upwards
    Use ShapeRenderer to draw player and based on Line type.

--- core/src/io/wasin/asteriods/states/Play.kt
+++ core/src/io/wasin/asteriods/states/Play.kt
@@ -2,6 +2,8 @@ package io.wasin.asteriods.states
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import io.wasin.asteriods.entities.Player
 import io.wasin.asteriods.handlers.GameStateManager
 
 /**
@@ -9,17 +11,29 @@ import io.wasin.asteriods.handlers.GameStateManager
  */
 class Play(gsm: GameStateManager): GameState(gsm){
 
+    private var sr: ShapeRenderer
+    private var player: Player
+
+    init {
+        sr = ShapeRenderer()
+        player = Player()
+    }
+
     override fun handleInput(dt: Float) {
     }
 
     override fun update(dt: Float) {
         handleInput(dt)
+
+        player.update(dt)
     }
 
     override fun render() {
         // clear screen
         Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        player.draw(sr)
     }
 
     override fun dispose() {
